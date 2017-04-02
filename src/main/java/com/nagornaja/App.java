67c5dc161581e62866ce7071493e9e43ce6c2aea
Nagornaya Elena <nitem91@ya.ru>
2017-04-03 04:13:51 +0700

    коммит.

--- src/main/java/com/nagornaja/App.java
+++ src/main/java/com/nagornaja/App.java
@@ -1,9 +1,69 @@
 package com.nagornaja;
 
-public class App 
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private final int GROUP_COUNT = 5;
+    private final int THREAD_COUNT = 2;
+    private final int ITEMS_COUNT = 50;
+
+    private int groupsCount;
+    private int threadsCount;
+    private int itemsCount;
+
+    private Producer producer;
+
+    private LinkedList<Item> queue = new LinkedList<>();
+
+    public App(){
+        new App(GROUP_COUNT, THREAD_COUNT, ITEMS_COUNT);
     }
+
+    public App(int groupsCount, int threadsCount, int itemsCount){
+        this.groupsCount = groupsCount;
+        this.threadsCount = threadsCount;
+        this.itemsCount = itemsCount;
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(LocalTime.now() + ": Start");
+
+        int groupsCount = Integer.decode(args[0]);
+        int threadsCount = Integer.decode(args[1]);
+        int itemsCount = Integer.decode(args[2]);
+
+        Producer producer = new Producer(groupsCount, itemsCount);
+        producer.start();
+
+        List<Consumer> consumers = new ArrayList<>();
+        for (int i = 0; i < threadsCount; i++){
+            consumers.add(new Consumer());
+        }
+
+        consumers.parallelStream().forEach(Consumer::start);
+
+        for(Consumer consumer : consumers){
+            try{
+                consumer.join();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        try{
+            producer.join();
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+
+        System.out.println(LocalTime.now() + ": Finish");
+
+    }
+
 }
--- src/main/java/com/nagornaja/Producer.java
+++ src/main/java/com/nagornaja/Producer.java
@@ -1,7 +1,37 @@
 package com.nagornaja;
 
+import java.time.LocalTime;
+import java.util.Random;
+
 /**
- * Created by Elene on 02.04.17.
+ * Класс, отвечающий за асинхронное произвдство объектов и добавление в очередь
+ * Created by Elene on 31.03.17.
  */
-public class Producer {
+public class Producer extends Thread{
+    private int groupsCount;
+    private int itemsCount;
+
+    private QueueService queueService = QueueService.getInstance();
+
+    public Producer(int groupsCount, int itemsCount){
+        this.groupsCount = groupsCount;
+        this.itemsCount = itemsCount;
+    }
+    @Override
+    public void run() {
+        Random random = new Random();
+        for(int i = 0; i < itemsCount; i++) {
+            Item item = new Item(random.nextInt(itemsCount), random.nextInt(groupsCount));
+            System.out.println(LocalTime.now() + ": CREATE ITEM: " + item);
+            queueService.putItem(item);
+            try {
+                Thread.sleep(1);
+            }
+            catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        queueService.finish();
+        System.out.println(LocalTime.now() + ": ADDING FINISHED.");
+    }
 }
